ec292cb080dc49652eefdaa4681d282fa3c74967
albugowy15 <kholidbughowi@gmail.com>
2023-07-08 11:43:42 +0700

    feat: parse team teaching class

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use sqlx::{mysql::MySqlPoolOptions, MySql, Pool};
 
-pub struct Connection {}
+pub struct Connection;
 
 impl Connection {
     pub async fn create_connection(db_url: &str) -> Result<Pool<MySql>> {
