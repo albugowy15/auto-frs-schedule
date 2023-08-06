82bebbcdf12147abdff47757368555c56bdf0b00
albugowy15 <kholidbughowi@gmail.com>
2023-08-06 13:22:34 +0700

    refactor: remove buffer writer

--- src/util.rs
+++ src/util.rs
@@ -1,48 +1,29 @@
 use std::path::PathBuf;
 
 use anyhow::{Context, Result};
-use tokio::{
-    fs::File,
-    io::{AsyncWriteExt, BufWriter},
-};
+use tokio::{fs::File, io::AsyncWriteExt};
 
 use crate::repo::Class;
 
 #[allow(deprecated)]
 pub async fn write_output(path_output: &PathBuf, list_class: &Vec<Class>) -> Result<()> {
-    let mut outfile = BufWriter::new(
-        File::create(path_output.as_path())
-            .await
-            .with_context(|| "Error creating output file")?,
-    );
-    let mut buffer = String::with_capacity(256);
+    let mut outfile = File::create(path_output.as_path()).await?;
 
     for class in list_class {
         let id_class = cuid::cuid().with_context(|| "Could not create cuid")?;
-        buffer.clear();
-        buffer.push_str(
-            "INSERT INTO Class (id, matkulId, day, code, isAksel, taken, sessionId) VALUES (\"",
+        let query = format!(
+            "INSERT INTO Class (id, matkulId, day, code, isAksel, taken, sessionId) VALUES ('{}', '{}', '{}', '{}', false, 0, {});\n",
+            id_class,
+            class.matkul_id,
+            class.day,
+            class.code,
+            class.session_id
         );
-        buffer.push_str(&id_class);
-        buffer.push_str("\", \"");
-        buffer.push_str(&class.matkul_id);
-        buffer.push_str("\", \"");
-        buffer.push_str(&class.day);
-        buffer.push_str("\", \"");
-        buffer.push_str(&class.code);
-        buffer.push_str("\", ");
-        buffer.push_str("false, 0, ");
-        buffer.push_str(&class.session_id.to_string());
-        buffer.push_str(");");
-        buffer.push('\n');
         outfile
-            .write_all(buffer.as_bytes())
+            .write_all(query.as_bytes())
             .await
-            .with_context(|| format!("Error writing to file: {}", buffer))?;
+            .with_context(|| format!("Error writing to file: {}", query))?;
     }
-    outfile
-        .flush()
-        .await
-        .with_context(|| "Error flushing output file")?;
+    outfile.sync_all().await?;
     Ok(())
 }
