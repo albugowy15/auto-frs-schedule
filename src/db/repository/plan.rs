3908be8b00b48bf58be4ce7a0a9af4d522ed423a
albugowy15 <kholidbughowi@gmail.com>
2024-02-19 18:34:57 +0700

    fix: sum totalSks error decode decimal type into int

--- src/db/repository/plan.rs
+++ src/db/repository/plan.rs
@@ -17,14 +17,20 @@ impl PlanRepository<'_> {
     pub async fn sync_total_sks(&self) -> Result<()> {
         let mut tx = self.db_pool.begin().await?;
         let rows = sqlx::query(
-            "select p.id, p.totalSks, sum(m.sks) as actual_sks from Plan p inner join _ClassToPlan cp on cp.B = p.id inner join Class c on cp.A = c.id inner join Matkul m on c.matkulId = m.id group by p.id having p.totalSks != sum(m.sks)",
+            "select p.id, p.totalSks, 
+            cast(sum(m.sks) as unsigned) as actual_sks from Plan p 
+            inner join _ClassToPlan cp on cp.B = p.id 
+            inner join Class c on cp.A = c.id 
+            inner join Matkul m on c.matkulId = m.id 
+            group by p.id having p.totalSks != sum(m.sks)",
         )
         .fetch_all(&mut *tx)
         .await?;
         println!("Sync totalSks {} plans", rows.len());
 
         for row in rows.into_iter() {
-            let actual_sks: i8 = row.get("actual_sks");
+            let actual_sks: u64 = row.get("actual_sks"); // declare as f32 since sum() has decimal
+                                                         // type
             let plan_id: String = row.get("id");
             sqlx::query("update Plan set totalSks = ? where id = ?")
                 .bind(actual_sks)
