e10cdec4e3b7d62ab53bea211b540783982e5781
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-08-13 12:14:18 +0700

    feat: print find schedule result with tabular format

--- src/commands/find.rs
+++ src/commands/find.rs
@@ -6,10 +6,17 @@ pub async fn find_handler(file: &PathBuf, sheet: &str, subject: &str) -> anyhow:
     let excel = Excel::new(file, sheet)?;
 
     let class_schedule = excel.find_schedule_from_class(subject);
+    println!(
+        "{0: <60} | {1: <35} | {2: <10} | {3: <10}",
+        "class", "lecturer codes", "day", "session"
+    );
     for schedule in class_schedule.into_iter() {
         println!(
-            "class:{}, lec_codes:{:?}, day: {}, session:{}",
-            schedule.class, schedule.lecturers_code, schedule.day, schedule.session_start
+            "{0: <60} | {1: <35} | {2: <10} | {3: <10}",
+            schedule.class,
+            format!("{:?}", schedule.lecturers_code),
+            schedule.day,
+            schedule.session_start
         );
     }
     Ok(())
